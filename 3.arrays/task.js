545d6d4e47dc986c34cbe05989173003eac371c4
Umed Sattorov <umasattory@gmail.com>
2023-01-07 02:50:53 +0300

    Chenges for Forked line

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,12 @@
 function compareArrays(arr1, arr2) {
-  
+  result = arr1.length === arr2.length && arr1.every((el, ind) => el === arr2[ind]);
+  return result; // boolean
 }
 
-function getUsersNamesInAgeRange(users, gender) {
-  
+function advancedFilter(arr) {
+  let resultArr = arr
+    .filter((nums) => nums > 0)
+    .filter((nums) => nums % 3 === 0)
+    .map((nums) => nums * 10);
+  return resultArr; // array
 }
