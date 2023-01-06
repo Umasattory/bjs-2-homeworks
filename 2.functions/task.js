f0200dd905d4889a9f6cfb5e8d7218501f34540e
Umed Sattorov <umasattory@gmail.com>
2023-01-07 02:48:52 +0300

    Change for Forked line

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,24 +1,58 @@
-function getArrayParams(...arr) {
-
+// Задание 1
+function getArrayParams(arr) {
+  let min = arr[0], max = arr[0], sum = 0, avg = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > max) {
+      max = arr[i]
+    }
+    if (arr[i] < min) {
+      min = arr[i]
+    }
+    //min = Math.min(...arr);
+    //max = Math.max(...arr);
+    //max = Math.max.apply(null, arr);
+    //min = Math.min.apply(null, arr);
+    sum += arr[i];
+    avg = sum / arr.length;
+    avg = Math.round(avg * 100) / 100;
+  };
   return { min: min, max: max, avg: avg };
 }
 
-function summElementsWorker(...arr) {
+// Задание 2
 
+function worker(arr) {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+  return sum;
 }
 
-function differenceMaxMinWorker(...arr) {
-
+function makeWork(arrOfArr, func) {
+  let max = func(arrOfArr[0]);
+  for (let i = 0; i < arrOfArr.length; i++) {
+    let sum = func(arrOfArr[i]);
+    if (sum > max) {
+      max = sum;
+    }
+  }
+  return max;
 }
 
-function differenceEvenOddWorker(...arr) {
-
-}
-
-function averageEvenElementsWorker(...arr) {
-
-}
-
-function makeWork (arrOfArr, func) {
-
+// Задание 3
+function worker2(arr) {
+  let min = arr[0];
+  let max = arr[0];
+  let differ = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] < min) {
+      min = arr[i];
+    }
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+    differ = Math.abs(max - min);
+  }
+  return differ;
 }
