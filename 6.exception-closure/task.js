a5abca6777dc3db289bca9f57ad9ea06e34a8237
Umed Sattorov <umasattory@gmail.com>
2023-02-02 19:49:58 +0300

    Correcting up to 02/02/2023

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -20,34 +20,35 @@ class Triangle {
     if (lineA > lineB + lineC ||
       lineB > lineA + lineC ||
       lineC > lineA + lineB) {
-      throw new Error("Ошибка! Треугольник не существует")
+      throw new Error("Треугольник с такими сторонами не существует")
     }
     this.lineA = lineA;
     this.lineB = lineB;
     this.lineC = lineC;
   }
   get perimeter() {
-    return String(this.lineA + this.lineB + this.lineC);
+    return this.lineA + this.lineB + this.lineC;
   }
 
   get area() {
     // p = half of perimeter
     const p = this.perimeter / 2;
     const areaTr = Math.sqrt(p * (p - this.lineA) * (p - this.lineB) * (p - this.lineC));
-    return String(Number(areaTr.toFixed(2)));
+    return Number(areaTr.toFixed(3));
   }
 };
 
 const getTriangle = (lineA, lineB, lineC) => {
   try {
-    const triangle = new Triangle(lineA, lineB, lineC);
-    return `${triangle.area} \n${triangle.perimeter}`
+    return new Triangle(lineA, lineB, lineC)
   } catch (err) {
-    return err;
+    return {
+      get perimeter() {
+        return "Ошибка! Треугольник не существует";
+      },
+      get area() {
+        return "Ошибка! Треугольник не существует";
+      }
+    }
   }
 };
-console.log("----------------------------------------------");
-console.log(validateCount('25jhlk'));
-console.log(getTriangle(2, 5, 5));
-console.log(getTriangle(5, 7, 9));
-console.log("----------------------------------------------");
